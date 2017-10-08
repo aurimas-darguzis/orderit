a313c868975bbb8218b0c1ace6fb37c72461f121
aurimas-darguzis <aurimas@darguzis.com>
2017-10-08 19:37:56 +0100

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.onload = function () {
 
   const lbl_title = document.getElementById('title');
   const sandwitch = document.getElementById('sandwitch');
+  const sandwitches = document.getElementById('sandwitches');
   
   const title = 'Order it!'
   lbl_title.innerHTML = title;
@@ -14,8 +15,10 @@ window.onload = function () {
   }
 
   function showSandwitches() {
-    const sandwitches = ['chicken', 'grain avocado', 'ham'];
-    
+    // const sandwitches = ['chicken', 'grain avocado', 'ham'];
+    sandwitches.classList.remove('hidden');
+    console.log(sandwitches)
+
   }
 
 }
