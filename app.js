dff2791ee9b459c377d4763e7d68d1c470f2b59e
aurimas-darguzis <aurimas@darguzis.com>
2017-10-08 21:02:27 +0100

    update

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ window.onload = function () {
   const sandwitch = document.getElementById('sandwitch');
   const sandwitches = document.getElementById('sandwitches');
   const sandwitch1 = document.getElementById('sandwitch1');
+  const sandwitch2 = document.getElementById('sandwitch2');
+  const sandwitch3 = document.getElementById('sandwitch3');
 
   const title = 'Order it!'
   lbl_title.innerHTML = title;
   sandwitch.innerHTML = 'sandwitch';
 
-  sandwitch.onclick = function(e) {
+  sandwitch.onclick = function(e) { // label `sandwitch`
     e.stopPropagation();
     console.log('sandwitch clicked!');
-    showSandwitches();
+    showSandwitches(); // shows list of sandwitches
   }
 
   sandwitch1.onclick = function(e) {
     e.stopPropagation();
-    const sandwitch1 = this;
-    addToBasket(sandwitch1);
+    this.classList.add('selected');
+    addToBasket(this);
+  }
+
+  sandwitch2.onclick = function(e) {
+    e.stopPropagation();
+    this.classList.add('selected');
+    addToBasket(this);
+  }
+
+  sandwitch3.onclick = function(e) {
+    e.stopPropagation();
+    this.classList.add('selected');
+    addToBasket(this);
+  }
+
+  sandwitch3.onclick = function(e) {
+    e.stopPropagation();
   }
 
   function showSandwitches() {
