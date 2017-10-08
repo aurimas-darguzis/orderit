585803247d9d6c2cf2f43ae98e0c20cf2cc51a53
aurimas-darguzis <aurimas@darguzis.com>
2017-10-08 19:00:29 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 window.onload = function () {
 
   const lbl_title = document.getElementById('title');
+  const sandwitch = document.getElementById('sandwitch');
+  
   const title = 'Order it!'
   lbl_title.innerHTML = title;
-  
+
+  sandwitch.onclick = function(e) {
+    e.stopPropagation();
+    console.log('sandwitch clicked!');
+
+    
+  }
+
 }
